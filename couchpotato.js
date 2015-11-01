543d0cfec4d2d6c1c0d3e17e6f368de26426cc83
Devin Buhl <devin.kray@gmail.com>
2015-11-01 12:58:01 -0500

    added error reply, cleaned up a bit

--- couchpotato.js
+++ couchpotato.js
@@ -8,17 +8,14 @@ var config = require('./config.json');
 
 var bot = new TelegramBot(config.telegram.botToken, { polling: true });
 
-/*
- * All available options
- */
 var couchpotato = new CouchPotatoAPI({
-		hostname: config.couchpotato.hostname, 
-		apiKey: config.couchpotato.apiKey,
-		port: config.couchpotato.port, 
-		urlBase: config.couchpotato.urlBase, 
-		ssl: config.couchpotato.ssl,
-		username: config.couchpotato.username,
-		password: config.couchpotato.password
+	hostname: config.couchpotato.hostname, 
+	apiKey: config.couchpotato.apiKey,
+	port: config.couchpotato.port, 
+	urlBase: config.couchpotato.urlBase, 
+	ssl: config.couchpotato.ssl,
+	username: config.couchpotato.username,
+	password: config.couchpotato.password
 });
 
 bot.onText(/\/search (.+)/, function (msg, match) {
@@ -27,10 +24,10 @@ bot.onText(/\/search (.+)/, function (msg, match) {
 
 	couchpotato.get("movie.search", { "q": movieName }).then(function (result) {
 		
+		// create keyboard choices
 		var keyboardOpts = [];
-	    _.forEach(result.movies, function(n, key) {
+	    	_.forEach(result.movies, function(n, key) {
 			keyboardOpts.push(new Array('/add ' + n.original_title + ' (' + n.year + '/' + n.imdb + ')'));
-			
 			if (key > 2) return false;
 		});
 
@@ -44,25 +41,22 @@ bot.onText(/\/search (.+)/, function (msg, match) {
 			"reply_markup": JSON.stringify({
 				"keyboard": keyboardOpts,
 			})
-	    };
+	    	};
 
-	    bot.sendMessage(chatId, 'Choose a movie to add:', opts);
+		bot.sendMessage(chatId, 'Choose a movie to add:', opts);
 	}).catch(function (err) {
+		bot.sendMessage(chatId, 'Error searching movie');
 		console.log(err);
 	});
 });
 
 bot.onText(/\/add (.+)([\d]{4})\/(tt[\d]+)/, function (msg, match) {
-	//console.log(msg.message_id);
-
 	var chatId = msg.chat.id;
 	var movieName = match[1].slice(0,-2);
 	var movieYear = match[2];
 	var movieId = match[3];
 
 	couchpotato.get("movie.add", { "identifier": movieId, "title": movieName }).then(function (result) {
-		// console.log('movie added: ' + movieName);
-	}).then(function() {
 		couchpotato.get("media.get", { "id": movieId }).then(function (result) {	
 			var moviePoster = result.media.info.images.poster[0];
 
@@ -78,9 +72,11 @@ bot.onText(/\/add (.+)([\d]{4})\/(tt[\d]+)/, function (msg, match) {
 
 			bot.sendMessage(chatId, '[Movie added!](' + moviePoster + ')', opts);
 		}).catch(function (err) {
+			bot.sendMessage(chatId, 'Error getting artwork');
 			console.log(err);
 		});	
 	}).catch(function (err) {
+		bot.sendMessage(chatId, 'Error adding movie');
 		console.log(err);
 	});
 });
