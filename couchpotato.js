568d7753a97538b108c090cca0f5086dd4724dad
Devin Buhl <devin.kray@gmail.com>
2015-11-01 21:26:35 -0500

    regress the profile feature for now, I'm tired ;)

--- couchpotato.js
+++ couchpotato.js
@@ -2,7 +2,6 @@
 
 var CouchPotatoAPI = require('./lib/couchpotato-api');
 var TelegramBot = require('node-telegram-bot-api');
-var Bluebird = require('bluebird');
 var _ = require("lodash");
 
 var config = require('./config.json');
@@ -19,28 +18,18 @@ var couchpotato = new CouchPotatoAPI({
 	password: config.couchpotato.password
 });
 
-/*
-search for movie name
- */
-bot.onText(/\/[sS]earch ([^\/p|P]+)\/{0,1}[p|P]{0,1}(.*)/, function (msg, match) {
-	
+bot.onText(/\/[Ss]earch (.+)/, function (msg, match) {
 	var chatId = msg.chat.id;
 	var movieName = match[1];
-	var profile = match[2] || '';
 
-	if (profile) {
-		profile = '/' + profile;
-	}
-
-	console.log('search: sending ' + chatId + ' a list of ' + movieName + ' matches');
-
-	couchpotato.get("movie.search", { "q": movieName })
-	.then(function (result) {
-
+	couchpotato.get("movie.search", { "q": movieName }).then(function (result) {
+		
+		console.log('search: sending ' + chatId + ' a request to add a movie ' + movieName);
+		
 		// create keyboard choices
 		var keyboardOpts = [];
-	    _.forEach(result.movies, function(n, key) {
-			keyboardOpts.push(new Array('/add ' + n.original_title + ' (' + n.year + '/' + n.imdb + profile + ')'));
+	    	_.forEach(result.movies, function(n, key) {
+			keyboardOpts.push(new Array('/add ' + n.original_title + ' (' + n.year + '/' + n.imdb + ')'));
 			if (key > 2) return false;
 		});
 
@@ -54,116 +43,49 @@ bot.onText(/\/[sS]earch ([^\/p|P]+)\/{0,1}[p|P]{0,1}(.*)/, function (msg, match)
 			"reply_markup": JSON.stringify({
 				"keyboard": keyboardOpts,
 			})
-	    };
+	    	};
 	    	
 		bot.sendMessage(chatId, 'Choose a movie to add:', opts);
-	})
-	.catch(function (err) {
+	}).catch(function (err) {
 		console.log(err);
 		bot.sendMessage(chatId, 'search: error searching movie');
 	});
 });
 
-/*
-add movie
- */
-bot.onText(/\/[aA]dd (.+)([\d]{4})\/(tt[\d]+)\/?(.+)?\)/, function (msg, match) {
+bot.onText(/\/add (.+)([\d]{4})\/(tt[\d]+)/, function (msg, match) {
 	var chatId = msg.chat.id;
 	var movieName = match[1].slice(0,-2);
 	var movieYear = match[2];
 	var movieId = match[3];
-	var profileName = match[4] || '';
 
-	console.log('add: sending ' + chatId + ' a request to add ' + movieName + ' (' + movieId + ')');
-
-	// send the request
-	Bluebird.join(_getProfile(profileName), _getThumbUrl(movieId), function(profile, thumb) {
-
-		couchpotato.get("movie.add", { "identifier": movieId, "title": movieName, "profile_id": profile[0]._id })
-		.then(function (result) {
-
-	    	var opts = {
+	couchpotato.get("movie.add", { "identifier": movieId, "title": movieName }).then(function (result) {
+		
+		console.log('add: sending ' + chatId + ' a request to add ' + movieName + ' (' + movieId + ')');
+		
+		couchpotato.get("media.get", { "id": movieId }).then(function (result) {	
+			
+			var moviePoster = result.media.info.images.poster[0];
+			
+			console.log('add: sending ' + chatId + ' a poster ' + moviePoster);
+			
+			var opts = {
 				"parse_mode": "Markdown",
 				"reply_to_message_id": msg.message_id,
 				"one_time_keyboard": true,
 				"resize_keyboard": false,
 				"selective": 2,
+				//"force_reply": true,
 				"reply_markup": JSON.stringify({ "hide_keyboard": true })
 			};
 
-			bot.sendMessage(chatId, '[Movie added!](' + thumb + ')', opts);
-
-		})
-		.catch(function (err) {
-			bot.sendMessage(chatId, 'add: error adding movie');
-
-			throw new Error("could not add movie: " + err);
+			bot.sendMessage(chatId, '[Movie added!](' + moviePoster + ')', opts);
+		}).catch(function (err) {
+			console.log(err);
+			bot.sendMessage(chatId, 'add: error getting artwork');
 		});
-	})
-	.catch(function (err) {
+	
+	}).catch(function (err) {
+		console.log(err);
 		bot.sendMessage(chatId, 'add: error adding movie');
-
-		throw new Error("could not add movie: " + err);
 	});
 });
-
-/*
-get profile names
- */
-bot.onText(/\/[pP]rofiles/, function (msg) {
-	var chatId = msg.chat.id;
-
-	console.log('profiles: sending ' + chatId + ' a request get profile names');
-
-	couchpotato.get("profile.list")
-	.then(function (result) {	
-		
-		var profileProfiles = [];
-		_.forEach(result.list, function(n, key) {
-			profileProfiles.push(n.label);
-		});
-
-		bot.sendMessage(chatId, profileProfiles.join(', '));
-		
-	})
-	.catch(function (err) {
-		bot.sendMessage(chatId, 'profiles: error grabbing profiles');
-
-		throw new Error("could not get profiles: " + err);
-	});
-});
-
-/*
-get profile_id by name
- */
-var _getProfile = function (profileName) {
-	return couchpotato.get("profile.list")
-	.then(function (result) {	
-		if (profileName) {
-			console.log('add: found profile_id for ' + profileName);
-			return  _.filter(result.list, function(item){
-	  			return item.label.match(new RegExp(profileName, 'i'));
-			});
-		} else {
-			return Object.keys(result.list)[0];
-		}
-	})
-	.catch(function (err) {
-		throw new Error("could not get profiles: " + err);
-	});
-};
-
-/*
-get thumbnail link
- */
-var _getThumbUrl = function (movieId) {
-	return couchpotato.get("media.get", { "id": movieId })
-	.then(function (result) {
-		var thumb =  result.media.info.images.poster[0]
-		return thumb;
-	})
-	.catch(function (err) {
-		throw new Error("could not get thumb url: " + err);
-	});	
-}
-
