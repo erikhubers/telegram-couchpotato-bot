461b1491f7ebd5e02b555f2dca351ca3bdfde75e
Travis Bybee <travis.bybee@gmail.com>
2015-12-29 12:42:42 -0700

    Bot will respond with custom keyboards
    * Change regex for movie and profile handling
    * Add `replyWithError` function

--- couchpotato.js
+++ couchpotato.js
@@ -5,6 +5,13 @@ var TelegramBot = require('node-telegram-bot-api');
 var NodeCache = require('node-cache');
 var _ = require('lodash');
 
+class Response {
+    constructor(message, keyboard) {
+      this.message = message;
+      this.keyboard = keyboard;
+    }
+}
+
 try {
   var config = require('./config.json');
 } catch (e) {
@@ -80,7 +87,8 @@ bot.onText(/\/[Qq](uery)? (.+)/, function(msg, match) {
       console.log(fromId + ' requested to search for movie ' + movieName);
 
       var movieList = [];
-      var response = ['*Found ' + movies.length + ' movies:*'];
+      var message = ['*Found ' + movies.length + ' movies:*'];
+      var keyboardList = [];
 
       _.forEach(movies, function(n, key) {
 
@@ -92,6 +100,7 @@ bot.onText(/\/[Qq](uery)? (.+)/, function(msg, match) {
         var thumb = ('images' in n ? ('poster' in n.images ? n.images.poster[0] : '') : '');
         var runtime = ('runtime' in n ? n.runtime : '');
         var onIMDb = ('via_imdb' in n ? true : false);
+        var keyboardValue = id + ") " + title + (year ? ' - ' + year : '')
 
         movieList.push({
           'id': id,
@@ -100,47 +109,56 @@ bot.onText(/\/[Qq](uery)? (.+)/, function(msg, match) {
           'rating': rating,
           'movie_id': movieId,
           'thumb': thumb,
-          'via_imdb': onIMDb
+          'via_imdb': onIMDb,
+          'keyboard_value': keyboardValue
         });
 
-        response.push(
+        message.push(
           '*' + id + '*) ' +
           (onIMDb ? '[' + title + '](http://imdb.com/title/' + movieId + ')' : '[' + title + '](https://www.themoviedb.org/movie/' + movieId + ')') +
           (year ? ' - _' + year + '_' : '') +
           (rating ? ' - _' + rating + '_' : '') +
           (runtime ? ' - _' + runtime + 'm_' : '')
         );
-      });
 
-      response.push('\n`/m [n]` to continue...');
+        keyboardList.push(
+          // One movie per row of custom keyboard
+          [keyboardValue]
+        )
+      });
 
       // set cache
       cache.set('movieList' + fromId, movieList);
 
-      return response.join('\n');
+      return new Response(message.join('\n'), keyboardList);
     })
     .then(function(response) {
       var opts = {
         'disable_web_page_preview': true,
         'parse_mode': 'Markdown',
         'selective': 2,
+        'reply_markup': {
+          'keyboard': response.keyboard,
+          'one_time_keyboard': true
+        }
       };
 
-      bot.sendMessage(chatId, response, opts);
+      bot.sendMessage(chatId, response.message, opts);
     })
     .catch(function(err) {
-      bot.sendMessage(chatId, 'Oh no! ' + err);
+      replyWithError(chatId, err)
     });
 
 });
 
 /*
 handle movie command
+in the form "1) Die Hard -- 1988", "2) Die Hard 2 -- 1990", etc.
  */
-bot.onText(/\/[mM](ovie)? ([\d]+)/, function(msg, match) {
+bot.onText(/([\d]+)\) .*/, function(msg, match) {
   var chatId = msg.chat.id;
   var fromId = msg.from.id;
-  var movieId = match[2];
+  var movieId = match[1].replace(')', '');
 
   // set movie option to cache
   cache.set('movieId' + fromId, movieId);
@@ -161,6 +179,8 @@ bot.onText(/\/[mM](ovie)? ([\d]+)/, function(msg, match) {
       console.log(fromId + ' requested to get profiles list');
 
       var profileList = [];
+      var keyboardList = [];
+      var keyboardRow = [];
       var response = ['*Found ' + profiles.length + ' profiles:*\n'];
       _.forEach(profiles, function(n, key) {
         profileList.push({
@@ -168,41 +188,54 @@ bot.onText(/\/[mM](ovie)? ([\d]+)/, function(msg, match) {
           'label': n.label,
           'hash': n._id
         });
+        // Keep profile id as an integer, convert to char for display
+        var keyAsChar = String.fromCharCode("A".charCodeAt(0) + key);
 
-        response.push('*' + (key + 1) + '*) ' + n.label);
+        response.push('*' + keyAsChar + '*) ' + n.label);
+
+        // Profile names are short, put two on each custom
+        // keyboard row to reduce scrolling
+        keyboardRow.push(keyAsChar + ") " + n.label);
+        if (keyboardRow.length == 2) {
+          keyboardList.push(keyboardRow);
+          keyboardRow = [];
+        }
       });
-
-      response.push('\n\n`/p [n]` to continue...');
 
       // set cache
       cache.set('movieProfileList' + fromId, profileList);
 
-      return response.join(' ');
+      return new Response(response.join(' '), keyboardList);
     })
     .then(function(response) {
-      bot.sendMessage(chatId, response, {
+      bot.sendMessage(chatId, response.message, {
         'selective': 2,
-        'parse_mode': 'Markdown'
+        'parse_mode': 'Markdown',
+        'reply_markup': {
+          'keyboard': response.keyboard,
+          'one_time_keyboard': true
+        }
       });
     })
     .catch(function(err) {
-      bot.sendMessage(chatId, 'Oh no! ' + err);
+      replyWithError(chatId, err)
     });
 });
 
 /*
 handle quality profile command
+in the form "A) HD", "B) SD", etc.
  */
-bot.onText(/\/[pP](rofile)? ([\d]+)/, function(msg, match) {
+bot.onText(/([A-Z])\) .*/, function(msg, match) {
   var chatId = msg.chat.id;
   var fromId = msg.from.id;
-  var profileId = match[2];
+  var profileId = match[1].replace(')', '').charCodeAt(0) - "A".charCodeAt(0) + 1;
   var profileList = cache.get('movieProfileList' + fromId);
   var movieId = cache.get('movieId' + fromId);
   var movieList = cache.get('movieList' + fromId);
 
   if (profileList === undefined || movieList === undefined || movieId === undefined) {
-    bot.sendMessage(chatId, 'Oh no! Error: something went wrong, try searching again');
+    throw new Error('Error: something went wrong, try searching again')
   }
 
   var movie = _.filter(movieList, function(item) {
@@ -228,11 +261,14 @@ bot.onText(/\/[pP](rofile)? ([\d]+)/, function(msg, match) {
 
       bot.sendMessage(chatId, '[Movie added!](' + movie.thumb + ')', {
         'selective': 2,
-        'parse_mode': 'Markdown'
+        'parse_mode': 'Markdown',
+        'reply_markup': {
+          'hide_keyboard': true
+        }
       });
     })
     .catch(function(err) {
-      bot.sendMessage(chatId, 'Oh no! ' + err);
+      replyWithError(chatId, err);
     })
     .finally(function() {
 
@@ -254,7 +290,7 @@ bot.onText(/\/searchwanted/, function(msg) {
     .then(function(result) {
       bot.sendMessage(chatId, 'Starting full search for all wanted movies.');
     }).catch(function(err) {
-      bot.sendMessage(chatId, 'Oh no! ' + err);
+      replyWithError(chatId, err);
     });
 });
 
@@ -271,3 +307,14 @@ bot.onText(/\/clear/, function(msg) {
 
   bot.sendMessage(chatId, 'All previously sent commands have been cleared, yey!');
 });
+
+/*
+ * Shared err message logic, primarily to handle removing the custom keyboard
+ */
+function replyWithError(chatId, err) {
+  bot.sendMessage(chatId, 'Oh no! ' + err, {  
+    'reply_markup': {
+      'hide_keyboard': true
+    }
+  });
+}
